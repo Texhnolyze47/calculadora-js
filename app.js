fde7fc00d0c421dda5726f2e2b67d133ae8133c2
Texhnolyze47 <ichigobankai136@gmail.com>
2023-01-20 12:14:03 -0600

    add: refactor event listener

--- app.js
+++ app.js
@@ -1,134 +1,130 @@
-class Calculator{
-  
-  constructor(displayValue,firstValue,waitingForSecondOperando,operator){
-      this.displayValue = displayValue;
-      this.firstValue = firstValue;
-      this.waitingForSecondOperando = waitingForSecondOperando
-      this.operator = operator;
-  };
+class Calculator {
+  constructor(displayValue, firstValue, waitingForSecondOperando, operator) {
+    this.displayValue = displayValue;
+    this.firstValue = firstValue;
+    this.waitingForSecondOperando = waitingForSecondOperando;
+    this.operator = operator;
+  }
 
-  reset(){
-    this.displayValue = '0';
+  reset() {
+    this.displayValue = "0";
     this.firstValue = null;
     this.waitingForSecondOperando = false;
     this.operator = null;
   }
 
-  
-  calculate(firstOperand,secondOperand, operator){
-    if(operator === '+'){
-      console.log(" El signo es " + operator)
-      console.log(" El resultado de la suma es "+  typeof(firstOperand) + typeof(secondOperand))
+  calculate(firstOperand, secondOperand, operator) {
+    if (operator === "+") {
+      console.log(" El signo es " + operator);
+      console.log(
+        " El resultado de la suma es " +
+          typeof firstOperand +
+          typeof secondOperand
+      );
 
       return firstOperand + secondOperand;
-    } else if (operator === '-'){
+    } else if (operator === "-") {
       return firstOperand - secondOperand;
-    } else if(operator === '*'){
+    } else if (operator === "*") {
       return firstOperand * secondOperand;
-    } else if(operator === '/'){
+    } else if (operator === "/") {
       return firstOperand / secondOperand;
     }
 
     return secondOperand;
   }
 
-  inputDigit(digit){
-    console.log("Calculadora" + calculator.waitingForSecondOperando)
+  inputDigit(digit) {
+    console.log("Calculadora" + calculator.waitingForSecondOperando);
 
-    if(this.waitingForSecondOperando === true){
-      console.log("Falta el operador")
-      console.log(digit)
-      this.displayValue = digit
+    if (this.waitingForSecondOperando === true) {
+      console.log("Falta el operador");
+      console.log(digit);
+      this.displayValue = digit;
       this.waitingForSecondOperando = false;
-    } else{
-      let displayValue = this.displayValue === '0' ? digit : this.displayValue + digit;
+    } else {
+      let displayValue =
+        this.displayValue === "0" ? digit : this.displayValue + digit;
 
-      this.displayValue = displayValue     
+      this.displayValue = displayValue;
     }
-    console.log(calculator)
+    console.log(calculator);
   }
 
   updateDisplay() {
-    const display = document.querySelector('#display');
-    console.log(display.value)
+    const display = document.querySelector("#display");
+    console.log(display.value);
     display.value = this.displayValue;
   }
 
-  inputDecimal(dot){
-    if(this.waitingForSecondOperando === true){
-      this.displayValue = '0.'
+  inputDecimal(dot) {
+    if (this.waitingForSecondOperando === true) {
+      this.displayValue = "0.";
       this.waitingForSecondOperando = false;
-      return
+      return;
     }
 
-    if(!this.displayValue.includes(dot)){
+    if (!this.displayValue.includes(dot)) {
       this.displayValue += dot;
     }
   }
 
-  handleOperator(nextOperator){
+  handleOperator(nextOperator) {
+    const inputValue = parseFloat(this.displayValue);
 
-    const inputValue = parseFloat(this.displayValue)
-
-    if(this.operator && this.waitingForSecondOperando){
+    if (this.operator && this.waitingForSecondOperando) {
       this.operator = nextOperator;
     }
 
-    if(this.firstValue === null && !isNaN(inputValue)){
+    if (this.firstValue === null && !isNaN(inputValue)) {
       this.firstValue = inputValue;
-    } else if (this.operator){
-      const result = this.calculate(this.firstValue, inputValue, this.operator)
-      console.log("El resultado es: " + result)
+    } else if (this.operator) {
+      const result = this.calculate(this.firstValue, inputValue, this.operator);
+      console.log("El resultado es: " + result);
 
       this.displayValue = `${parseFloat(result.toFixed(7))}`;
       this.firstValue = result;
     }
 
     this.waitingForSecondOperando = true;
-    this.operator = nextOperator
-    console.log(calculator)
-
-
+    this.operator = nextOperator;
+    console.log(calculator);
   }
-
 }
 
-const calculator = new Calculator('0',null,false, null)
+const numeros = document.querySelector(".container-botones");
+
+const calculator = new Calculator("0", null, false, null);
 calculator.updateDisplay();
 
+numeros.addEventListener("click", (event) => {
+  const button = event.target;
+  const buttonValue = button.innerText;
 
-const numeros = document.querySelector('.container-botones');
-
-numeros.addEventListener('click', (event) => {
-  const key  = event.target;
-
-  if(!key.matches('button')){
+  if (!button.matches("button")) {
+    console.log("boton");
     return;
   }
 
-  if(key.classList.contains('signos')){
-    calculator.handleOperator(key.textContent)
-    calculator.updateDisplay()
-    return;
+  switch (buttonValue) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+    case "=":
+      calculator.handleOperator(buttonValue);
+      break;
+    case ".":
+      calculator.inputDecimal(buttonValue);
+      break;
+    case "borrar":
+      calculator.reset();
+      break;
+    default:
+      if (Number.isInteger(parseFloat(buttonValue))) {
+        calculator.inputDigit(buttonValue);
+      }
   }
 
-  
-  if(key.classList.contains('decimal')){
-    calculator.inputDecimal(key.textContent)
-    calculator.updateDisplay();
-    return;
-  }
-
- 
-  if(key.classList.contains('borrar')){
-    calculator.reset()
-    calculator.updateDisplay();
-    return;
-  }
-
-  calculator.inputDigit(key.textContent);
-  calculator.updateDisplay()
-
-
+  calculator.updateDisplay();
 });
-
