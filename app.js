4ff2ccb496316a4c80f2dd141b92d792ea5e8297
Texhnolyze47 <ichigobankai136@gmail.com>
2023-01-20 11:15:35 -0600

    add: se agrego las funcionaes basicas de suma,resta,multiplicacion,division y borrar

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
-class ModelCalculator{
+class Calculator{
   
   constructor(displayValue,firstValue,waitingForSecondOperando,operator){
       this.displayValue = displayValue;
       this.firstValue = firstValue;
-      this.WaitingForSecondOperando = waitingForSecondOperando
+      this.waitingForSecondOperando = waitingForSecondOperando
       this.operator = operator;
   };
 
-
-  inputDigit(digit){
-    let displayValue = this.displayValue === '0' ? digit : this.displayValue + digit;
-
-    this.displayValue = displayValue 
+  reset(){
+    this.displayValue = '0';
+    this.firstValue = null;
+    this.waitingForSecondOperando = false;
+    this.operator = null;
   }
 
+  
+
+  calculate(firstOperand,secondOperand, operator){
+    if(operator === '+'){
+      console.log(" El signo es " + operator)
+      console.log(" El resultado de la suma es "+  typeof(firstOperand) + typeof(secondOperand))
+
+      return firstOperand + secondOperand;
+    } else if (operator === '-'){
+      return firstOperand - secondOperand;
+    } else if(operator === '*'){
+      return firstOperand * secondOperand;
+    } else if(operator === '/'){
+      return firstOperand / secondOperand;
+    }
+
+    return secondOperand;
+  }
+
+  inputDigit(digit){
+    console.log("Calculadora" + calculator.waitingForSecondOperando)
+
+    if(this.waitingForSecondOperando === true){
+      console.log("Falta el operador")
+      console.log(digit)
+      this.displayValue = digit
+      this.waitingForSecondOperando = false;
+    } else{
+      let displayValue = this.displayValue === '0' ? digit : this.displayValue + digit;
+
+      this.displayValue = displayValue     
+    }
+    console.log(calculator)
+  }
 
   updateDisplay() {
     const display = document.querySelector('#display');
+    console.log(display.value)
     display.value = this.displayValue;
   }
 
@@ -26,12 +61,36 @@ class ModelCalculator{
     }
   }
 
+  handleOperator(nextOperator){
+
+    const inputValue = parseFloat(this.displayValue)
+
+    if(this.operator && this.waitingForSecondOperando){
+      this.operator = nextOperator;
+    }
+
+    if(this.firstValue === null && !isNaN(inputValue)){
+      this.firstValue = inputValue;
+    } else if (this.operator){
+      const result = this.calculate(this.firstValue, inputValue, this.operator)
+      console.log("El resultado es: " + result)
+
+      this.displayValue = String(result)
+      this.firstValue = result;
+    }
+
+    this.waitingForSecondOperando = true;
+    this.operator = nextOperator
+    console.log(calculator)
+
+
+  }
+
 }
 
-
-const calculator = new ModelCalculator('0',null,false, null)
-
+const calculator = new Calculator('0',null,false, null)
 calculator.updateDisplay();
+
 
 const numeros = document.querySelector('.container-botones');
 
@@ -43,13 +102,13 @@ numeros.addEventListener('click', (event) => {
   }
 
   if(key.classList.contains('signos')){
-    console.log('signos', key.textContent)
+    calculator.handleOperator(key.textContent)
+    calculator.updateDisplay()
     return;
   }
 
   
   if(key.classList.contains('decimal')){
-    console.log('decimal', key.textContent)
     calculator.inputDecimal(key.textContent)
     calculator.updateDisplay();
     return;
@@ -57,13 +116,14 @@ numeros.addEventListener('click', (event) => {
 
  
   if(key.classList.contains('borrar')){
-    console.log('clear', key.textContent);
-
+    calculator.reset()
+    calculator.updateDisplay();
     return;
   }
 
   calculator.inputDigit(key.textContent);
   calculator.updateDisplay()
+
 
 });
 
