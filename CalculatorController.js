f331be93144c395760845f3614d490bd37daaaff
Texhnolyze47 <ichigobankai136@gmail.com>
2023-01-23 12:32:38 -0600

    Se termino de cambiar los valores booleanos a constantes

--- CalculatorController.js
+++ CalculatorController.js
@@ -6,7 +6,7 @@ import { WAITING_FOR_FIRST_OPERATOR, WAITING_FOR_SECOND_OPERATOR } from "./state
 export default class CalculatorController{
 
     constructor(){
-    this.calculatorModel = new CalculatorModel("0", null, false, null);
+    this.calculatorModel = new CalculatorModel("0", null, WAITING_FOR_FIRST_OPERATOR, null);
     this.calculatorView = new CalculatorView();
     this.calculatorService = new CalculatorService();
     }
@@ -14,7 +14,7 @@ export default class CalculatorController{
     inputDecimal(dot){
       if (this.calculatorModel.waitingForSecondOperator === WAITING_FOR_SECOND_OPERATOR) {
         this.calculatorModel.firstDisplayValue = "0.";
-        this.calculatorModel.waitingForSecondOperator = false;
+        this.calculatorModel.waitingForSecondOperator = WAITING_FOR_FIRST_OPERATOR;
         return;
       }
   
@@ -30,7 +30,7 @@ export default class CalculatorController{
             console.log("Operator is Missing");
             console.log(digit);
             this.calculatorModel.firstDisplayValue = digit;
-            this.calculatorModel.waitingForSecondOperator = false;
+            this.calculatorModel.waitingForSecondOperator = WAITING_FOR_FIRST_OPERATOR;
           } else {
             let displayValue =
             this.calculatorModel.firstDisplayValue === "0" ? digit : this.calculatorModel.firstDisplayValue + digit;
@@ -51,7 +51,7 @@ export default class CalculatorController{
       reset(){
         this.calculatorModel.firstDisplayValue = "0";
         this.calculatorModel.secondDisplayValue = null;
-        this.calculatorModel.waitingForSecondOperator = false;
+        this.calculatorModel.waitingForSecondOperator = WAITING_FOR_FIRST_OPERATOR;
         this.calculatorModel.operator = null;
         this.calculatorModel.tempExpression = "";
         this.calculatorView.displayValue.value = "0"
@@ -106,7 +106,7 @@ export default class CalculatorController{
       this.calculatorView.operationDisplay.value =  "√" + "(" + this.calculatorModel.firstDisplayValue + ")";
       this.calculatorModel.firstDisplayValue = `${parseFloat(result.toFixed(7))}`;
       this.calculatorModel.secondDisplayValue = result;
-      this.calculatorModel.waitingForSecondOperator = false;
+      this.calculatorModel.waitingForSecondOperator = WAITING_FOR_FIRST_OPERATOR;
       this.calculatorModel.operator = null;
     } else if (this.calculatorModel.secondDisplayValue === null && !isNaN(inputValue)) {
       this.calculatorModel.secondDisplayValue = inputValue;
