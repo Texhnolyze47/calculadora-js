3a5b85a30a8191b33faaec9da39c9114739a3447
Texhnolyze47 <ichigobankai136@gmail.com>
2023-01-23 22:49:23 -0600

    Revert "Codigo innecesario de CalculatorModel"
    This reverts commit c6c6313f060f50cae5d52a48d801b16e259f6d2d.

--- CalculatorController.js
+++ CalculatorController.js
@@ -2,64 +2,36 @@ import CalculatorModel  from "./CalculatorModel.js";
 import CalculatorView from "./CalculatorView.js";
 import CalculatorService from "./CalculatorService.js";
 
-/**
- * CalculatorController class handles the actions of the calculator and 
- * is responsible for managing the view and the model.
- * @class
- */
 export default class CalculatorController{
-    /**
-     * Creates an instances of CalculatorController
-     * @constructor
-     */
+
     constructor(){
     this.calculatorModel = new CalculatorModel();
     this.calculatorView = new CalculatorView();
     this.calculatorService = new CalculatorService();
     }
 
-    /**
-     * handle the decimal button click event
-     * @param {string} dot - decimal value
-     */
     handleDecimalButton(dot){
       this.calculatorModel.inputDecimal(dot)
     }
 
-    /**
-     * Handle the backspace button click event
-     * @param {number} digit  - digit value
-     */
     handleDigitButton(digit){
       this.calculatorModel.inputDigit(digit);
     }
 
-    /**
-     * Handle the backspace button event
-     */
     handleBackspaceButton(){
       this.calculatorModel.backspace()
     }
 
-    /**
-     * Handle the reset button event
-     */
     handleResetButton(){
       this.calculatorModel.reset()
       this.calculatorView.reset()
     }
     
-    /**
-     * Update the display
-     */
     handleUpdateDisplay() {
      this.calculatorView.updateDisplay(this.calculatorModel);
   }
-    /**
-     * Handle the operator button click event
-     * @param {string} nextOperator - the operator that was clicked
-     */
-    handleOperator(nextOperator) {
+
+  handleOperator(nextOperator) {
     const inputValue = parseFloat(this.calculatorModel.firstDisplayValue);
     const result = this.calculatorService.calculate(this.calculatorModel.secondDisplayValue,inputValue,this.calculatorModel.operator);
 
--- CalculatorView.js
+++ CalculatorView.js
@@ -1,35 +1,16 @@
-
-/**
- * CalculatorView class is responsible for managing the view of the calculator
- * it updates the display values of the calculator and resets the calculator
- * @class
- */
 export default class CalculatorView{
 
-    /**
-     * Creates an instances of CalculatorView
-     * @constructor
-     */
     constructor(){
-        /** Element of the display value of the calculator
-         *  @type {HTMLElement} */
         this.displayValue = document.querySelector("#display");
         this.operationDisplay = document.querySelector("#operation");
         this.btnClear = document.querySelector(".clearAll");
     }
 
-    /**
-     * Resets teh calculator's display values
-     */
     reset(){
         this.displayValue.value = "0"
         this.operationDisplay.value = ""
     }
 
-    /**
-     * updates the display values of calculator
-     * @param {CalculatorModel} calculatorModel 
-     */
     updateDisplay(calculatorModel) {
         this.operationDisplay.innerHTML = calculatorModel.tempExpression;
         this.displayValue.value = calculatorModel.firstDisplayValue
